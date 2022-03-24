ff076f58362f0f27d5cd1167a149eac173d3cbd1
dangtrungduc1993 <trungduc.hou@gmail.com>
2022-03-24 16:22:29 +0700

    baitap

---
+++ TH-khai-bao-su-dung-bien/src/main2.js
@@ -0,0 +1,6 @@
+var width2;
+var height2;
+width2 = 10.5;
+height2 = 40;
+var area2 = width2 * height2;
+console.log("Dien tich la: ".concat(area2));
---
+++ TH-khai-bao-su-dung-bien/src/main2.ts
@@ -0,0 +1,7 @@
+let width2: number;
+let height2: number;
+width2 = 10.5;
+height2 = 40;
+let area2: number = width2 * height2;
+console.log(`Dien tich la: ${area2}`);
+let value :(number|string);
